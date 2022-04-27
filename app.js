d9b0113ca2e81e1083b452fb0fd911bdfa8726c7
Rubens Christoff <rubenschristoff@gmail.com>
2022-04-27 00:21:24 -0300

    Route Users Add

--- app.js
+++ app.js
@@ -1,40 +1,85 @@
 'use strict'
 import Hapi from '@hapi/hapi'
 import Joi from 'joi'
-//import vision from 'vision'
-//import inert from 'inert'
-//import lout from 'lout'
+import db from './db/db.js'
 const app = Hapi.server({ port: 3000, host: 'localhost' })
 
-//await app.register([vision,inert,lout])
 
 app.route(
     {
         method: 'GET',
         path: '/',
-        handler: (request, h) => { return 'Hello World!' },
+        handler: (request, h) => { return 'Welcome toDo API!' },
     }
 
 );
 
-//endpoint todos for as a new task
+//route users
+app.route({
+    method: ['POST','PUT'],
+        path: '/users',
+        handler: (request, reply) => {
+            const payload = request.payload
+
+           db('users').insert({
+                name: payload.name,
+                surname: payload.surname,
+                password: payload.password
+            }).catch((err) =>{
+                console.log(err)
+            })
+
+            /*reply.response({
+                "name": payload.name,
+                "surname": payload.surname,
+            }).code(200)*/
+        },
+        //validation with Joi
+        options: {
+            validate: {
+                payload: Joi.object ({
+                    name: Joi.string().min(3).required(),
+                    surname: Joi.string().min(3).required(),
+                    password: Joi.string().min(6).pattern(new RegExp('^[a-zA-Z0-9]{6,30}$')).required(),
+                })
+            }
+        }
+})
+
+
+//route todos put
 app.route(
     {
         method: ['POST','PUT'],
         path: '/todos',
         handler: (request, h) => {
-            console.log(request.payload)
             const payload = request.payload
 
-            if (!payload) {
-                return h.response('descrição obrigatoria').code(422)
+            db('todos').insert({
+                state: payload.state,
+                description: payload.description
+            }).catch((err) =>{
+                console.log(err)
+            })
+
+
+        },
+        //validation with Joi
+        options: {
+            validate: {
+                payload: Joi.object ({
+                    state: Joi.string().default('INCOMPLETE') ,
+                    description: Joi.string().required(),
+                })
+            },
+            response:{
+                
             }
-            return h.response(`Criado ${payload.description}`).code(200)
         }
     }
 )
 
-//endpoint GET /todos route
+//route todos get
 app.route(
     {
         method: 'GET',
